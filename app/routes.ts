7f3fbea00f4bdbc59574900e7223fd63616a0beb
Juha Komulainen <juha.komulainen@evident.fi>
2016-03-02 10:25:47 +0200

    Make naming more consistent

--- app/routes.ts
+++ app/routes.ts
@@ -3,11 +3,11 @@ export function routeConfiguration($routeProvider: angular.route.IRouteProvider)
     $routeProvider
         .when('/', { template: '<homepage></homepage>'})
         .when('/foo', { template: '<foo></foo>'})
-        .when('/hello/:target', { template: '<hello target="{{routeParams.target}}"></hello>', controller: exposeRouteParametersController })
+        .when('/hello/:target', { template: '<hello target="{{routeParams.target}}"></hello>', controller: exposeRouteParamsController })
         .otherwise( { template: '<not-found></not-found>'});
 }
 
-exposeRouteParametersController.$inject = ['$routeParams', '$scope'];
-function exposeRouteParametersController($routeParams: any, $scope: any) {
+exposeRouteParamsController.$inject = ['$routeParams', '$scope'];
+function exposeRouteParamsController($routeParams: any, $scope: any) {
     $scope.routeParams = $routeParams;
 }
